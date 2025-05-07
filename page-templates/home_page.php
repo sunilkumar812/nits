c9d9890436becf6fe3967b33d6328b91d51990bb
Anas Choudhary <anas9719@gmail.com>
2025-05-08 03:39:51 +0530

    Partial Separator H3 done

--- page-templates/home_page.php
+++ page-templates/home_page.php
@@ -9,9 +9,16 @@
  */
 get_header();
 ?>
-<main id="primary" class="site-main">
+<main id="primary" class="site-main z-4 relative overflow-hidden">
     <!-- Hero Section -->
     <?= get_template_part('partials/sections/partial', 'hero'); ?>
+
+    <!-- Separator H3 -->
+    <?= get_template_part('partials/core/partial', 'separator_h3', [
+        'class_container' => 'px-4 max-w-5xl mx-auto mt-20 mb-16',
+        'class_text' => 'text-xl md:text-xl font-semibold text-nitsSteelBlue text-center',
+        'text' => 'Trusted by the top automotive brands',
+    ]); ?>
 </main>
 <?php
 get_footer();
---
+++ partials/core/partial-separator_h3.php
@@ -0,0 +1,12 @@
+<?php
+$class_container = $args['class_container'];
+$class_text = $args['class_text'];
+$text = isset($args['text']) ? $args['text'] : 'Headline Text';
+?>
+<div class="main-width relative z-4 <?= $class_container ?>">
+    <div class="flex items-center justify-center gap-4 lg:gap-16">
+        <div class="min-w-4 lg:min-w-[103px] h-px bg-nitsBluePlus"></div>
+        <h3 class="<?= $class_text ?>"><?= $text ?></h3>
+        <div class="min-w-4 lg:min-w-[103px] h-px bg-nitsBluePlus"></div>
+    </div>
+</div>
